e0cca4d40b49633ead1c2d1c13c7011f8f252879
vlakir <vek2006_73@mail.ru>
2021-08-19 17:16:25 +0300

    bugfix++

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ requirements = ["tornado>=6.1"]
 
 setup(
     name="cleanapi",
-    version="0.1.2",
+    version="0.1.4",
     author="Vladimir Kirievskiy",
     author_email="vlakir1234@gmail.com",
     description="Pretty tornado wrapper for making lightweight REST API services",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/vlakir/cleanapi.git",
+    # packages=['cleanapi', 'cleanapi.logger', 'cleanapi.third_party_libs'],
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
